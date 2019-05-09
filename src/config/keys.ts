5eb4eb22b83cebb78d5d93c61e936b5376285bf6
Michael Wilson <wilson.michael.c@gmail.com>
2019-05-09 14:43:52 -0400

    Update keys.ts

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -3,7 +3,7 @@ export default {
     mongoURI: process.env.DB_URL ?  `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_URL}:${process.env.DB_PORT}/usersdb?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0` : 'mongodb://localhost/profile',
     cognitoConfig: {
         region: 'us-east-1',
-        cognitoUserPoolId: 'us-east-1_kwVrr8Ugs',
+        cognitoUserPoolId: 'us-east-1_TDue1hDQ2',
         tokenUse: 'id',
         tokenExpiration: 3600000,
     },
